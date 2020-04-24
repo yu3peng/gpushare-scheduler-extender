5c5b02bf5a7434cdde74d1dd774cf0a80be0b0c8
yupeng <yu3peng@qq.com>
2020-04-24 14:46:50 +0800

    Update bind.go

--- pkg/scheduler/bind.go
+++ pkg/scheduler/bind.go
@@ -1,7 +1,7 @@
 package scheduler
 
 import (
-	"github.com/AliyunContainerService/gpushare-scheduler-extender/pkg/cache"
+	"gpushare-scheduler-extender/pkg/cache"
 	"k8s.io/apimachinery/pkg/types"
 	schedulerapi "k8s.io/kubernetes/pkg/scheduler/api"
 )
