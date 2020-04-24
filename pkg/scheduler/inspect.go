2971f1ec15c67585191796e3e5dd135c3886fe47
yupeng <yu3peng@qq.com>
2020-04-24 14:48:46 +0800

    Update inspect.go

--- pkg/scheduler/inspect.go
+++ pkg/scheduler/inspect.go
@@ -1,8 +1,8 @@
 package scheduler
 
 import (
-	"github.com/AliyunContainerService/gpushare-scheduler-extender/pkg/cache"
-	"github.com/AliyunContainerService/gpushare-scheduler-extender/pkg/utils"
+	"gpushare-scheduler-extender/pkg/cache"
+	"gpushare-scheduler-extender/pkg/utils"
 )
 
 func (in Inspect) Handler(name string) *Result {
