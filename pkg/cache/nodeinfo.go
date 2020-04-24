8f6a30725b0fa8ec5651cf8504a9ac608759593c
yupeng <yu3peng@qq.com>
2020-04-24 14:44:43 +0800

    Update nodeinfo.go

--- pkg/cache/nodeinfo.go
+++ pkg/cache/nodeinfo.go
@@ -5,7 +5,7 @@ import (
 	"log"
 	"sync"
 
-	"github.com/AliyunContainerService/gpushare-scheduler-extender/pkg/utils"
+	"gpushare-scheduler-extender/pkg/utils"
 	"k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
